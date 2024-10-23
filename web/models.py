a7ea8b6c94acfef4ff18783023524290f55d96e3
Cristian Gallego <info@megadominio.co>
2024-10-23 13:46:37 -0500

    Modulo web

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 from bas.models import *
 
+# modelo de banners
+class Web(ClaseModelo):
+    titulo = models.CharField(max_length=100, verbose_name="Título de la Web")
+    
+
+# modelo de banners
+class Banners(ClaseModelo):
+    titulo = models.CharField(max_length=100, verbose_name="Título de la Imagen")
+    imagen = models.ImageField(upload_to='imagenes/album/imagenes/', verbose_name="Imagen")
+
+    def __str__(self):
+        return self.titulo
+
